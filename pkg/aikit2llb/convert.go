64cfd4ce25f0e2b487d785a72c2461bb96459ba8
Sertac Ozercan <sozercan@gmail.com>
2024-01-04 17:58:53 +0000

    chore: update to localai v2.4.0
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- pkg/aikit2llb/convert.go
+++ pkg/aikit2llb/convert.go
@@ -17,7 +17,7 @@ const (
 	debianSlim     = "docker.io/library/debian:12-slim"
 	distrolessBase = "gcr.io/distroless/cc-debian12:latest"
 
-	localAIVersion = "v2.3.1"
+	localAIVersion = "v2.4.0"
 	localAIRepo    = "https://github.com/mudler/LocalAI"
 	cudaVersion    = "12-3"
 )
