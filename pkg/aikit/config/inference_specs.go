4b3d068cda087221de89373bc0a4c94ac3dda947
Sertac Ozercan <sozercan@gmail.com>
2024-04-12 21:42:55 +0000

    update
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- pkg/aikit/config/inference_specs.go
+++ pkg/aikit/config/inference_specs.go
@@ -3,21 +3,21 @@ package config
 
 type InferenceConfig struct {
 	APIVersion string   `yaml:"apiVersion" json:"apiVersion" jsonschema:"required"`
-	Debug      bool     `yaml:"debug" json:"debug"`
-	Runtime    string   `yaml:"runtime" json:"runtime"`
-	Backends   []string `yaml:"backends" json:"backends"`
+	Debug      bool     `yaml:"debug,omitempty" json:"debug,omitempty"`
+	Runtime    string   `yaml:"runtime,omitempty" json:"runtime,omitempty"`
+	Backends   []string `yaml:"backends,omitempty" json:"backends,omitempty"`
 	Models     []Model  `yaml:"models" json:"models" jsonschema:"required"`
-	Config     string   `yaml:"config" json:"config"`
+	Config     string   `yaml:"config,omitempty" json:"config,omitempty"`
 }
 
 type Model struct {
-	Name            string           `yaml:"name" json:"name"`
-	Source          string           `yaml:"source" json:"source"`
-	SHA256          string           `yaml:"sha256" json:"sha256"`
-	PromptTemplates []PromptTemplate `yaml:"promptTemplates" json:"promptTemplates"`
+	Name            string           `yaml:"name" json:"name" jsonschema:"required"`
+	Source          string           `yaml:"source" json:"source" jsonschema:"required"`
+	SHA256          string           `yaml:"sha256,omitempty" json:"sha256,omitempty"`
+	PromptTemplates []PromptTemplate `yaml:"promptTemplates,omitempty" json:"promptTemplates,omitempty"`
 }
 
 type PromptTemplate struct {
-	Name     string `yaml:"name" json:"name"`
-	Template string `yaml:"template" json:"template"`
+	Name     string `yaml:"name,omitempty" json:"name,omitempty"`
+	Template string `yaml:"template,omitempty" json:"template,omitempty"`
 }
