f1215d74be7bf6e5a71358e536bda6a573e71be6
Sertac Ozercan <sozercan@gmail.com>
2024-05-17 03:18:05 +0000

    update
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- pkg/aikit2llb/inference/exllama.go
+++ pkg/aikit2llb/inference/exllama.go
@@ -15,13 +15,13 @@ func installExllama(c *config.InferenceConfig, s llb.State, merge llb.State) llb
 	}
 
 	savedState := s
-	s = s.Run(utils.Sh("apt-get update && apt-get install --no-install-recommends -y git ca-certificates python3-pip python3-dev python3-venv make g++ curl && curl -LsSf https://astral.sh/uv/install.sh | sh && pip install grpcio-tools --break-system-packages && apt-get clean"), llb.IgnoreCache).Root()
+	s = s.Run(utils.Sh("apt-get update && apt-get install --no-install-recommends -y bash git ca-certificates python3-pip python3-dev python3-venv make g++ curl && curl -LsSf https://astral.sh/uv/install.sh | sh && pip install grpcio-tools --break-system-packages && apt-get clean"), llb.IgnoreCache).Root()
 
 	// clone localai exllama backend only
 	s = cloneLocalAI(s, backend)
 
 	// clone exllama to localai exllama backend path and install python dependencies
-	s = s.Run(utils.Shf("cd /tmp/localai/backend/python/%[1]s && rm -rf .git && . $HOME/.cargo/env && python3 -m grpc_tools.protoc -I../.. --python_out=. --grpc_python_out=. backend.proto && export BUILD_TYPE=cublas && sh -c ./install.sh", backend)).Root()
+	s = s.Run(utils.Shf("cd /tmp/localai/backend/python/%[1]s && rm -rf .git && . $HOME/.cargo/env && python3 -m grpc_tools.protoc -I../.. --python_out=. --grpc_python_out=. backend.proto && export BUILD_TYPE=cublas && bash -c ./install.sh", backend)).Root()
 
 	diff := llb.Diff(savedState, s)
 	return llb.Merge([]llb.State{merge, diff})
