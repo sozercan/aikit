5b874629763af8f8b1320b88d6acf5955b854d5f
Sertac Ozercan <sozercan@gmail.com>
2024-05-20 04:38:30 +0000

    update-s

--- pkg/aikit2llb/inference/exllama.go
+++ pkg/aikit2llb/inference/exllama.go
@@ -24,8 +24,10 @@ func installExllama(c *config.InferenceConfig, s llb.State, merge llb.State) llb
 	// clone localai exllama backend only
 	s = cloneLocalAI(s)
 
+	s = s.Run(utils.Bashf("source $HOME/.cargo/env && export BUILD_TYPE=cublas && ./install.sh")).Root()
+
 	// clone exllama to localai exllama backend path and install python dependencies
-	s = s.Run(utils.Bashf("git clone --depth 1 %[1]s --branch %[2]s /tmp/%[3]s && mv /tmp/%[3]s/* /tmp/localai/backend/python/%[3]s && rm -rf /tmp/%[3]s && cd /tmp/localai/backend/python/%[3]s && rm -rf .git && source $HOME/.cargo/env && python3 -m grpc_tools.protoc -I../.. --python_out=. --grpc_python_out=. backend.proto && uv venv && source .venv/bin/activate && ls -al && uv pip install --no-build-isolation --requirement requirements-install.txt && EXLLAMA_NOCOMPILE= uv pip install --no-build-isolation --requirement requirements.txt", exllamaRepo, exllamaTag, backend)).Root()
+	// s = s.Run(utils.Bashf("git clone --depth 1 %[1]s --branch %[2]s /tmp/%[3]s && mv /tmp/%[3]s/* /tmp/localai/backend/python/%[3]s && rm -rf /tmp/%[3]s && cd /tmp/localai/backend/python/%[3]s && rm -rf .git && source $HOME/.cargo/env && python3 -m grpc_tools.protoc -I../.. --python_out=. --grpc_python_out=. backend.proto && uv venv && source .venv/bin/activate && ls -al && uv pip install --no-build-isolation --requirement requirements-install.txt && EXLLAMA_NOCOMPILE= uv pip install --no-build-isolation --requirement requirements.txt", exllamaRepo, exllamaTag, backend)).Root()
 
 	diff := llb.Diff(savedState, s)
 	return llb.Merge([]llb.State{merge, diff})
