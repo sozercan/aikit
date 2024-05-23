8e1a22c2ed9d23ed5f4c5c73b021e46b7114a649
Sertac Ozercan <sozercan@gmail.com>
2024-05-23 20:47:07 +0000

    updates
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- pkg/aikit2llb/inference/convert.go
+++ pkg/aikit2llb/inference/convert.go
@@ -201,12 +201,12 @@ func installMamba(s llb.State, merge llb.State) llb.State {
 
 func installOpenCV(s llb.State, merge llb.State) llb.State {
 	savedState := s
-	// // adding debian 11 (bullseye) repo due to opencv 4.5 requirement
-	// s = s.Run(utils.Sh("echo 'deb http://deb.debian.org/debian bullseye main' | tee -a /etc/apt/sources.list")).Root()
-	// // pinning libdap packages to bullseye version due to symbol error
-	// libdapVersion := "3.20.7-6"
-	// libPath := "/usr/lib/x86_64-linux-gnu"
-	s = s.Run(utils.Shf("apt-get update && mkdir -p /tmp/generated/images && apt-get install -y libopencv-imgcodecs406 libgomp1 libdap27 libdapclient6v5 && apt-get clean"), llb.IgnoreCache).Root()
+	// adding debian 11 (bullseye) repo due to opencv 4.5 requirement
+	s = s.Run(utils.Sh("echo 'deb http://deb.debian.org/debian bullseye main' | tee -a /etc/apt/sources.list")).Root()
+	// pinning libdap packages to bullseye version due to symbol error
+	libdapVersion := "3.20.7-6"
+	libPath := "/usr/lib/x86_64-linux-gnu"
+	s = s.Run(utils.Shf("apt-get update && mkdir -p /tmp/generated/images && apt-get install -y libopencv-imgcodecs4.5 libgomp1 libdap27=%[1]s libdapclient6v5=%[1]s && apt-get clean && ln -s %[2]s/libopencv_core.so.4.5 %[2]s/libopencv_core.so.4.5d && ln -s %[2]s/libopencv_core.so.4.5 %[2]s/libopencv_core.so.406 && ln -s %[2]s/libopencv_imgcodecs.so.4.5 %[2]s/libopencv_imgcodecs.so.4.5d", libdapVersion, libPath), llb.IgnoreCache).Root()
 	diff := llb.Diff(savedState, s)
 	merge = llb.Merge([]llb.State{merge, diff})
 
