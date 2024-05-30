4b8c1e65eef0273fda02fe89931d03486dd1f81f
Sertac Ozercan <sozercan@gmail.com>
2024-05-30 22:20:11 +0000

    docs: update baseUrl due to helm chart
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://sozercan.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/aikit',
+  baseUrl: '/aikit/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
