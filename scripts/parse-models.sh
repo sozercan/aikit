5314e34a7120c23e16138985d352da1ebf21f633
Sertaç Özercan <852750+sozercan@users.noreply.github.com>
2024-07-27 22:30:26 -0700

    feat: add llama 3.1 8b and 70b (#344)

--- scripts/parse-models.sh
+++ scripts/parse-models.sh
@@ -2,13 +2,13 @@
 
 # Define the functions to extract each part
 extract_model_name() {
-    # Capture the base name, exclude size and type
-    echo "$1" | sed -E 's/^([a-z]+)-([0-9]+x[0-9]+b|[0-9]+\.?[0-9]*b)-.*/\1/' | sed -E 's/^([a-z]+)-([0-9]+)-.*/\1\2/' | sed -E 's/^([a-z]+)-([0-9]+\.?[0-9]*b)$/\1/'
+    # Capture the base name, handling the special case for llama-3.1
+    echo "$1" | sed -E 's/^llama-(3\.1)-([0-9]+\.?[0-9]*b)-.*/llama\1/;t; s/^([a-z]+)-([0-9]+x[0-9]+b|[0-9]+\.?[0-9]*b)-.*/\1/; s/^([a-z]+)-([0-9]+)-.*/\1\2/; s/^([a-z]+)-([0-9]+\.?[0-9]*b)$/\1/'
 }
 
 extract_model_size() {
-    # Capture the size part (e.g., 7b, 13b, 8x7b)
-    echo "$1" | sed -E 's/^[a-z]+-([0-9]+x[0-9]+b|[0-9]+\.?[0-9]*b)-.*/\1/' | sed -E 's/^[a-z]+-[0-9]+-([0-9]+\.?[0-9]*b).*/\1/' | sed -E 's/^[a-z]+-([0-9]+\.?[0-9]*b)$/\1/'
+    # Capture the size part, handling the special case for llama-3.1
+    echo "$1" | sed -E 's/^llama-(3\.1)-([0-9]+\.?[0-9]*b)-.*/\2/;t; s/^[a-z]+-([0-9]+x[0-9]+b|[0-9]+\.?[0-9]*b)-.*/\1/; s/^[a-z]+-[0-9]+-([0-9]+\.?[0-9]*b).*/\1/; s/^[a-z]+-([0-9]+\.?[0-9]*b)$/\1/'
 }
 
 extract_model_type() {
@@ -17,7 +17,7 @@ extract_model_type() {
 }
 
 # Run and display results for each example
-for MODEL in "llama-2-7b-chat" "llama-2-13b-chat" "llama-3-8b-instruct" "phi-3-3.8b" "gemma-2b-instruct" "codestral-22b" "llama-3-70b-instruct" "mixtral-8x7b-instruct"; do
+for MODEL in "llama-2-7b-chat" "llama-2-13b-chat" "llama-3-8b-instruct" "llama-3.1-8b-instruct" "phi-3-3.8b" "gemma-2b-instruct" "codestral-22b" "llama-3-70b-instruct" "llama-3.1-70b-instruct" "mixtral-8x7b-instruct"; do
     echo "Model: $MODEL"
     echo "  Name: $(extract_model_name $MODEL)"
     echo "  Size: $(extract_model_size $MODEL)"
