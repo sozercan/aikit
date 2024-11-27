a2d422c549e9a3d3d5cf8ff961e7dea70f649038
Sertac Ozercan <sozercan@gmail.com>
2024-11-27 01:21:06 +0000

    fix: bump grpcio to 1.68.0
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- pkg/aikit2llb/inference/diffusers.go
+++ pkg/aikit2llb/inference/diffusers.go
@@ -11,7 +11,7 @@ func installDiffusers(s llb.State, merge llb.State) llb.State {
 
 	s = cloneLocalAI(s)
 
-	s = s.Run(utils.Bashf("export BUILD_TYPE=cublas && export CUDA_MAJOR_VERSION=12 && cd /tmp/localai/backend/python/%[1]s && sed -i 's/grpcio==1.66.0/grpcio==1.66.1/g' requirements.txt && make %[1]s", utils.BackendDiffusers)).Root()
+	s = s.Run(utils.Bashf("export BUILD_TYPE=cublas && export CUDA_MAJOR_VERSION=12 && cd /tmp/localai/backend/python/%[1]s && sed -i 's/grpcio==1.67.1/grpcio==1.68.0/g' requirements.txt && make %[1]s", utils.BackendDiffusers)).Root()
 
 	diff := llb.Diff(savedState, s)
 	return llb.Merge([]llb.State{merge, diff})
--- pkg/aikit2llb/inference/exllama.go
+++ pkg/aikit2llb/inference/exllama.go
@@ -11,7 +11,7 @@ func installExllama(s llb.State, merge llb.State) llb.State {
 
 	s = cloneLocalAI(s)
 
-	s = s.Run(utils.Bashf("export BUILD_TYPE=cublas && export CUDA_MAJOR_VERSION=12 && cd /tmp/localai/backend/python/%[1]s && make %[1]s", utils.BackendExllamaV2)).Root()
+	s = s.Run(utils.Bashf("export BUILD_TYPE=cublas && export CUDA_MAJOR_VERSION=12 && cd /tmp/localai/backend/python/%[1]s && sed -i 's/grpcio==1.67.1/grpcio==1.68.0/g' requirements.txt && make %[1]s", utils.BackendExllamaV2)).Root()
 
 	diff := llb.Diff(savedState, s)
 	return llb.Merge([]llb.State{merge, diff})
