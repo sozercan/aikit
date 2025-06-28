2cb779fe242b4ba1c93fe9a51d3e7482b9f50a70
Sertac Ozercan <sozercan@gmail.com>
2025-06-28 22:09:58 +0000

    revert
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- pkg/aikit2llb/inference/image.go
+++ pkg/aikit2llb/inference/image.go
@@ -35,7 +35,6 @@ func emptyImage(c *config.InferenceConfig, platform *specs.Platform) *specs.Imag
 	img.Config.Env = []string{
 		"PATH=" + system.DefaultPathEnv(utils.PlatformLinux),
 		"CONFIG_FILE=/config.yaml",
-		"LOCALAI_SKIP_LIBRARY_PATH=1",
 	}
 
 	cudaEnv := []string{
