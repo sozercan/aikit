0062adfa86c626d66ca7ec199ae47a3895bf42e9
Sertac Ozercan <sozercan@gmail.com>
2025-07-12 01:14:30 +0000

    test
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- pkg/aikit2llb/inference/vllm.go
+++ pkg/aikit2llb/inference/vllm.go
@@ -8,7 +8,7 @@ import (
 
 func installVLLM(c *config.InferenceConfig, s llb.State, merge llb.State) llb.State {
 	savedState := s
-	s = s.Run(utils.Sh("apt-get update && apt-get install --no-install-recommends -y bash git ca-certificates python3-pip python3-dev python3-venv python-is-python3 make g++ curl && pip install uv grpcio-tools && apt-get clean"), llb.IgnoreCache).Root()
+	s = s.Run(utils.Sh("apt-get update && apt-get install --no-install-recommends -y bash git ca-certificates python3-pip python3-dev python3-venv python-is-python3 make g++ curl && pip install uv grpcio-tools==1.59.0 && apt-get clean"), llb.IgnoreCache).Root()
 
 	s = cloneLocalAI(s)
 
@@ -16,7 +16,7 @@ func installVLLM(c *config.InferenceConfig, s llb.State, merge llb.State) llb.St
 		s = s.AddEnv("BUILD_TYPE", "cublas").AddEnv("CUDA_MAJOR_VERSION", "12")
 	}
 
-	s = s.Run(utils.Bashf("cd /tmp/localai/backend/python/%[1]s && sed -i 's/grpcio==1.67.1/grpcio==1.68.1/g' requirements.txt && make %[1]s", utils.BackendVLLM)).Root()
+	s = s.Run(utils.Bashf("cd /tmp/localai/backend/python/%[1]s && make %[1]s", utils.BackendVLLM)).Root()
 
 	diff := llb.Diff(savedState, s)
 	return llb.Merge([]llb.State{merge, diff})
